60eff639c9046caa762b667dc6c86d65759d483c
2023popn <nathanp2023@gmail.com>
2023-08-27 12:21:41 -0400

    modified:   index.js 	modified:   node_modules/.package-lock.json

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ const app = express();
 const port = process.env.PORT;
 
 /*
-    Setting up PostgreSQL
-*/
-const { Client } = require('pg')
-const client = new Client({
-  user: 'flare_db_user',
-  host: 'dpg-cjlnarlk5scs739sq9b0-a',
-  database: 'flare_db',
-  password: '99VOQIDJNnKBnJUTrxRfiLD13hqTPQWe',
-  port: 5432,
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-/*
     Send html informaiton to client
 */
 const {readFile} = require('fs').promises;
