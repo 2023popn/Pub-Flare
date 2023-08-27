43205129cdedf1b5268443b3cf2f9981956a2113
2023popn <nathanp2023@gmail.com>
2023-08-27 12:17:58 -0400

    modified:   index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT;
 
+/*
+    Setting up PostgreSQL
+*/
+const { Client } = require('pg')
+const client = new Client({
+  user: 'flare_db_user',
+  host: 'dpg-cjlnarlk5scs739sq9b0-a',
+  database: 'flare_db',
+  password: '99VOQIDJNnKBnJUTrxRfiLD13hqTPQWe',
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+/*
+    Send html informaiton to client
+*/
 const {readFile} = require('fs').promises;
 
 app.get("/", async (request, response) => {
